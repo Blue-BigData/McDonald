9af92734b0e75a495cd2c5b2d21bd8134dca3a7f
Blue-BigData <122299114+Blue-BigData@users.noreply.github.com>
2023-06-26 20:21:43 +0900

    McDonald's Store Reviews.py

--- McDonald's Store Reviews.py
+++ McDonald's Store Reviews.py
@@ -1,7 +1,1 @@
 # McDonald Project
-from google.colab import drive
-drive.mount('/content/drive')
-
-import pandas as pd
-data = pd.read_csv(f"/content/drive/MyDrive/SNU-COSS-2023/McDonald_s_Reviews.csv", encoding='cp1252')
-df = data.copy()
